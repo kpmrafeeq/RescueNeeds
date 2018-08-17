6633e4fcd0922ee04505f09a1fd37a377c32f94f
Mohamed Rafeeq <Mohamed.Palanchery@flydubai.com>
2018-08-18 01:59:06 +0400

    Updated

---
+++ RescueNeeds/App_Start/AuthorizeUserAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RescueNeeds.App_Start
+{
+    public class AuthorizeUserAttribute : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("/account/login");
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["Logged"] == "true")
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+     
+
+    }
+}
---
+++ RescueNeeds/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RescueNeeds.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == ConfigurationManager.AppSettings["username"] && password == ConfigurationManager.AppSettings["password"])
+            {
+                Session["Logged"] = "true";
+                return RedirectToAction("index", "home");
+            }
+            else
+            {
+                Session["Logged"] = "false";
+                return RedirectToAction("login", "account");
+            }
+        }
+    }
+}
